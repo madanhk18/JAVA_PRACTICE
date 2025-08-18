a1c0afeb0177f539cf06f2b00e336fe8b40b48e9
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-08-18 19:53:18 +0530

    Commits

---
+++ demo48.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class demo48 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter nAme:");
+        String name=sc.nextLine();
+        System.out.println("name is "+name);
+    }
+}
