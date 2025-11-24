4026aafa04b4ccc9118aff5383085f8e9c05af01
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-11-24 16:39:25 +0530

    github daily push

--- d158.java
+++ d158.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class d158 {
+    public static void main(String[] args) {
+        String s= "Start Coding Here ";
+        String newS=s.trim();
+        System.out.println(s);
+        System.out.println(newS);
+        String []arr=newS.split(" ");
+        System.out.println(arr[arr.length-1].length());
+    }
 }
