ecace882d3866c5e6279315af3920a2ccdf545f7
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-11-24 16:39:56 +0530

    github daily push

--- d159.java
+++ d159.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class d159 {
-}
+    public static void main(String[] args) {
+        String s="abcd";
+        findAllSubseuence(s);
+    }
+    public static void findAllSubseuence(String s) {
+    }
+    }
