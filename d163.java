b5e43884146627302018a37326f3d34444672e1e
Madan H K <109542799+madanhk18@users.noreply.github.com>
2025-11-27 17:18:05 +0530

    Day commit

--- d163.java
+++ d163.java
@@ -1,4 +1,34 @@
-package PACKAGE_NAME;
-
 public class d163 {
+    public static void main(String[] args) {
+        /*String Problem: Valid Anagram
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+Example 1:
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+Input: s = "rat", t = "car"
+Output: false
+*/
+        String s = "rat";
+        String t = "car";
+        checkAnagram(s,t);
+
+    }
+    public static void checkAnagram(String s,String t){
+        int arr[]=new int[26];
+        for (char ch:s.toCharArray()){
+            arr[ch - 'a']++;
+        }
+        for (char ch:t.toCharArray()) {
+            if (arr[ch - 'a'] <= 0) {
+                System.out.println("false");
+                break;
+            }
+            else {
+                System.out.println("true");
+                break;
+            }
+        }
+
+    }
 }
